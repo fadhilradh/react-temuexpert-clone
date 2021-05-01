657ab693e10fd19bf7d02aa442cd8c200db900ec
fadhilradh <fadhilradh@users.noreply.github.com>
2021-05-01 09:23:56 +0700

    add tailwind and materialUI

---
+++ src/layout/MainLayout.js
@@ -0,0 +1,33 @@
+import React from "./react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
+
+class MainLayout extends React.Component {
+   render() {
+      const { classes } = this.props;
+      return (
+         <AppBar position="static">
+            <Toolbar>
+               <IconButton
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+               >
+                  <MenuIcon />
+               </IconButton>
+               <Typography variant="h6" className={classes.title}>
+                  News
+               </Typography>
+               <Button color="inherit">Login</Button>
+            </Toolbar>
+         </AppBar>
+      );
+   }
+}
+
+export default MainLayout;
