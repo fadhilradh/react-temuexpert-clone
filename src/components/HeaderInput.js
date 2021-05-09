d35e2205d2f5b1edb4a8758036c7905021d1f04e
fadhilradh <fadhilradh@users.noreply.github.com>
2021-05-09 16:08:29 +0700

    refactor to function & hooks

--- src/components/HeaderInput.js
+++ src/components/HeaderInput.js
@@ -1,22 +1,14 @@
 import TextField from "@material-ui/core/TextField";
-import React from "react";
+import React, { useState } from "react";
 
-class HeaderInput extends React.Component {
-   state = {
-      expertName: "",
-   };
+const HeaderInput = () => {
+   const [expertName, setExpertName] = useState("");
 
-   render() {
-      return (
-         <form noValidate autoComplete="off" margin="dense">
-            <TextField
-               borderRadius="50%"
-               label="Cari Expert"
-               variant="outlined"
-            />
-         </form>
-      );
-   }
-}
+   return (
+      <form noValidate autoComplete="off" margin="dense">
+         <TextField borderRadius="50%" label="Cari Expert" variant="outlined" />
+      </form>
+   );
+};
 
 export default HeaderInput;
--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-class Footer extends React.Component {
-   render() {
-      return (
-         <footer className="bg-blue-20 h-auto md:h-80 pt-10 pb-10 text-white">
-            <div className="max-w-screen-xl mx-auto">
-               <div className="flex justify-center px-5 md:px-20 xs:px-8 md:text-left mx-auto tracking-wider"></div>
-            </div>
-         </footer>
-      );
-   }
-}
+const Footer = () => {
+   return (
+      <footer className="bg-blue-20 h-auto md:h-80 pt-10 pb-10 text-white">
+         <div className="max-w-screen-xl mx-auto">
+            <div className="flex justify-center px-5 md:px-20 xs:px-8 md:text-left mx-auto tracking-wider"></div>
+         </div>
+      </footer>
+   );
+};
 
 export default Footer;
