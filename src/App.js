aee998324cfd96edffccd4a0b3b958c3b4e8dafa
fadhilradh <fadhilradh@users.noreply.github.com>
2021-05-09 16:19:16 +0700

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./layout/Header.js";
 import Footer from "./layout/Footer.js";
+import HomePage from "./pages/index.js";
 
 function App() {
    return (
       <div className="App">
          <Header />
+         <HomePage />
          <Footer />
       </div>
    );
