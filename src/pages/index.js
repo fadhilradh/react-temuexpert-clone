d52695990179a2775ace64721579743115743873
fadhilradh <fadhilradh@users.noreply.github.com>
2021-05-10 18:15:27 +0700

    add homepage details

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,31 @@ const HomePage = () => {
                className="w-4/5 md:w-1/2 mx-auto mt-5 md:mt-0 "
             ></img>
          </section>
+
+         <section class="flex flex-col md:flex-row full-width justify-center mb-16 md:mb-36 max-w-screen-xl">
+            <div
+               class="text-h4 mb-12 w-full text-center px-10"
+               id="temuExpertSteps"
+            >
+               <span>Cukup 3 Langkah </span>
+               <span class="text-primary text-bold">Mudah</span>
+            </div>
+            <div class="flex sm:flex-row flex-col md:w-3/4 w-full justify-around text-center">
+               <div
+                  v-for="(step,index) in stepsDetail"
+                  class="flex flex-col items-center md:w-1/4 w-full mb-16"
+               >
+                  <div
+                     class="flex items-center justify-center border-blue-600 border rounded-full
+                    -16 w-16 font-bold text-h4 text-secondary"
+                  >
+                     {}
+                  </div>
+                  <img alt="" src="step.src" class="my-4 w-1/2 md:w-full" />
+                  <div class="text-18 text-center md:w-full w-2/3">{}</div>
+               </div>
+            </div>
+         </section>
       </div>
    );
 };
